1071f3ccaf6cfe39de27446a7e55b1cdc05cde9f
leesiyen <leesiyen02012@naver.com>
2025-03-10 10:52:50 +0900

    초기커밋

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public PlayerController controller;
+    public PlayerCondition condition;
+
+    public ItemData itemData;
+    public Action addItem;
+
+    private void Awake()
+    {
+        CharacterManager.Instance.Player = this;
+        controller = GetComponent<PlayerController>();
+        condition = GetComponent<PlayerCondition>();
+    }
+
+}
